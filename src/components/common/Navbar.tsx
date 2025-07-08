acbb72ab4c6de9b297c719f3d26a9610e44adfb6
Rakibul Hasan <rakibulhasan745101@gmail.com>
2025-07-08 20:28:33 +0600

    navbar responsive

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,18 +2,18 @@
 "use client";
 
 import { BookMarked, PlusCircle, BarChart3 } from "lucide-react";
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import { ModeToggle } from "../mode-toggle";
 
 export function Navbar() {
 
     return (
-        <nav className="w-full px-6 py-4 bg-background border-b shadow-sm flex justify-between items-center">
+        <nav className="w-full px-6 py-4 bg-background border-b shadow-sm flex justify-between items-center gap-4 flex-col sm:flex-row">
             <Link to="/" className="text-xl font-bold tracking-wide">
                 📚 BookStack
             </Link>
 
-            <ul className="flex space-x-6 text-sm items-center">
+            <ul className="flex gap-2 space-x-6 text-sm items-center flex-col sm:flex-row">
                 <li>
                     <Link to="/books" className="flex items-center gap-1 hover:text-primary transition">
                         <BookMarked className="h-4 w-4" />
